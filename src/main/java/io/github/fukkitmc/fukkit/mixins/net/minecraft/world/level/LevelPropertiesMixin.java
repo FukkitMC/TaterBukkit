1738b7a6427441557f208f24183d0cb7a41d7920
hYdos <haydenv06@gmail.com>
2020-05-30 09:30:45 +1000

    works again

--- src/main/java/io/github/fukkitmc/fukkit/mixins/net/minecraft/world/level/LevelPropertiesMixin.java
+++ src/main/java/io/github/fukkitmc/fukkit/mixins/net/minecraft/world/level/LevelPropertiesMixin.java
@@ -1,12 +1,12 @@
 package io.github.fukkitmc.fukkit.mixins.net.minecraft.world.level;
 
+import io.github.fukkitmc.fukkit.extras.LevelPropertiesExtra;
 import net.minecraft.world.level.LevelProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(LevelProperties.class)
-public abstract class LevelPropertiesMixin{
-
+public abstract class LevelPropertiesMixin implements LevelPropertiesExtra {
 
     @Shadow public String levelName;
 
@@ -15,4 +15,6 @@ public abstract class LevelPropertiesMixin{
             this.levelName = name;
         }
     }
+
+
 }
