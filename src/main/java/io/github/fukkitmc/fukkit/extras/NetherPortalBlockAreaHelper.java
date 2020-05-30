74809c5ef5647a4c24c6c39afbe531d72b0ab40c
Ramid Khan <ramidzkh@gmail.com>
2020-05-30 10:11:57 +1000

    Can't find LecternInventory. Fix or remove.

---
+++ src/main/java/io/github/fukkitmc/fukkit/extras/NetherPortalBlockAreaHelper.java
@@ -0,0 +1,9 @@
+package io.github.fukkitmc.fukkit.extras;
+
+/**
+ * Extra for {@link net.minecraft.block.NetherPortalBlock.AreaHelper}
+ */
+public interface NetherPortalBlockAreaHelper {
+
+    boolean createPortal();
+}
