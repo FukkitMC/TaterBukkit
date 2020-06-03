667c0fe5723e9f08cac8efb2eb4aaa7f19c21c09
hYdos <haydenv06@gmail.com>
2020-06-03 16:34:00 +1000

    force myself to fix tps

--- src/main/java/io/github/fukkitmc/fukkit/mixins/net/minecraft/server/network/ServerLoginNetworkHandlerMixin.java
+++ src/main/java/io/github/fukkitmc/fukkit/mixins/net/minecraft/server/network/ServerLoginNetworkHandlerMixin.java
@@ -1,49 +1,19 @@
 package io.github.fukkitmc.fukkit.mixins.net.minecraft.server.network;
 
-import com.mojang.authlib.GameProfile;
-import net.minecraft.network.ClientConnection;
-import net.minecraft.server.MinecraftServer;
+import io.github.fukkitmc.fukkit.extras.ServerLoginNetworkHandlerExtra;
 import net.minecraft.server.network.ServerLoginNetworkHandler;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ServerLoginNetworkHandler.class)
-public abstract class ServerLoginNetworkHandlerMixin {
+public abstract class ServerLoginNetworkHandlerMixin implements ServerLoginNetworkHandlerExtra {
 
-    @Shadow public ServerLoginNetworkHandler.State state;
+    @Shadow public abstract void disconnect(Text reason);
 
-    @Shadow public abstract void acceptPlayer();
-
-    @Shadow public MinecraftServer server;
-
-    @Shadow @Final public ClientConnection connection;
-
-    @Shadow public ServerPlayerEntity clientEntity;
-
-    @Shadow public GameProfile profile;
-
-
-    /**
-     * @author No
-     */
-    @Overwrite
-    public void tick() {
-        if (this.state == ServerLoginNetworkHandler.State.READY_TO_ACCEPT) {
-            this.acceptPlayer();
-        } else if (this.state == ServerLoginNetworkHandler.State.DELAY_ACCEPT) {
-            ServerPlayerEntity serverPlayerEntity = this.server.getPlayerManager().getPlayer(this.profile.getId());
-            if (serverPlayerEntity == null) {
-                this.state = ServerLoginNetworkHandler.State.READY_TO_ACCEPT;
-                this.server.getPlayerManager().onPlayerConnect(this.connection, this.clientEntity);
-                this.clientEntity = null;
-            }
-        }
-
+    @Override
+    public void disconnect(String var0) {
+        this.disconnect(new LiteralText(var0));
     }
-
 }
