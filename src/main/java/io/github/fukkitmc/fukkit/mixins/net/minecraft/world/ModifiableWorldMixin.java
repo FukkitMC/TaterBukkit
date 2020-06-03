2d0700cf6e3687040b58245d60ff59e3b005e37d
hYdos <haydenv06@gmail.com>
2020-06-03 15:55:56 +1000

    yes

---
+++ src/main/java/io/github/fukkitmc/fukkit/mixins/net/minecraft/world/ModifiableWorldMixin.java
@@ -0,0 +1,15 @@
+package io.github.fukkitmc.fukkit.mixins.net.minecraft.world;
+
+import io.github.fukkitmc.fukkit.extras.ModifiableWorldExtra;
+import net.minecraft.world.ModifiableWorld;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ModifiableWorld.class)
+public interface ModifiableWorldMixin extends ModifiableWorldExtra {
+
+    @Override
+    default boolean addEntity(net.minecraft.entity.Entity var0, org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason var1){
+        return true;
+    }
+
+}
