1f0efff196ddb030da46c09f83637cc656694867
hYdos <haydenv06@gmail.com>
2020-06-05 06:56:19 +1000

    Update RegistryMixin.java

--- src/main/java/io/github/fukkitmc/fukkit/mixins/net/minecraft/util/registry/RegistryMixin.java
+++ src/main/java/io/github/fukkitmc/fukkit/mixins/net/minecraft/util/registry/RegistryMixin.java
@@ -2,7 +2,9 @@ package io.github.fukkitmc.fukkit.mixins.net.minecraft.util.registry;
 
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.bukkit.craftbukkit.enchantments.CraftEnchantment;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentWrapper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,7 +16,7 @@ public class RegistryMixin {
     @Inject(method = "register(Lnet/minecraft/util/registry/Registry;Lnet/minecraft/util/Identifier;Ljava/lang/Object;)Ljava/lang/Object;", at = @At("TAIL"))
     private static <T> void register(Registry<? super T> registry, Identifier id, T entry, CallbackInfoReturnable<T> cir){
         if(registry == Registry.ENCHANTMENT){
-            Enchantment.registerEnchantment((Enchantment) entry);
+            Enchantment.registerEnchantment(new CraftEnchantment((net.minecraft.enchantment.Enchantment) entry));
         }
     }
 
